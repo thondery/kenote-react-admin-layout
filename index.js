ab6a6fe138a3a317870e1dc51fd90b995b5a1e86
thondery <thondery@163.com>
2017-11-17 14:13:23 +0800

    Reinitialize the component ...

--- index.js
+++
@@ -1,4 +0,0 @@
-require('./assets/styles/index.scss')
-exports.Layout = require('./lib/components/layout').default
-exports.Header = require('./lib/components/header').default
-exports.Sider = require('./lib/components/sider').default
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Layout } from './layout'
-export { default as Header } from './header'
-export { default as Sider } from './sider'
